9bfaf8d3d1027ab4c5d57a08532fe42cc58a7fb3
Morten Albertsen <morten.albertsen@me.com>
2019-03-21 18:49:40 +0100

    Updated modelclass to betetr suit reality of dataset, specifically changed required properties to optionals

--- beer_review.py
+++ beer_review.py
@@ -3,18 +3,17 @@ from datetime import datetime
 
 
 class BeerReview(db.Entity):
-
     brewery_id = Required(int)
-    brewery_name = Required(str)
+    brewery_name = Required(unicode)
     review_time = Required(datetime)
     overall = Required(float)
     aroma = Required(float)
     appearance = Required(float)
-    profilename = Required(str)
-    beer_style = Required(str)
+    profilename = Optional(unicode)
+    beer_style = Required(unicode)
     palate = Required(float)
     taste = Required(float)
-    beer_name = Required(str)
-    # Got no idea what _abv is...
-    beer_abv = Required(float)
+    beer_name = Required(unicode)
+    # Got no idea what _abv is...not required either
+    beer_abv = Optional(float)
     beer_id = Required(int)
