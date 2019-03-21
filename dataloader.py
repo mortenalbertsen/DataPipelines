3cf14d726def5aba7370aa2adf12239426e73967
Morten Albertsen <morten.albertsen@me.com>
2019-03-21 18:50:30 +0100

    Commit for every 2000 (batch_size) insert, updates to reflect changes in model, BeerReview

--- dataloader.py
+++ dataloader.py
@@ -1,51 +1,69 @@
-import csv
+import unicodecsv
 from database import connect
 from pony import db_session
+from pony.orm import commit
 from beer_review import BeerReview
-import io
+from datetime import datetime
 
-
-@db_session
-def load_csv_entries_into_db(csv_file):
-    entries = read_csv_file(csv_file)
-
-    for entry in entries:
-        BeerReview(brewery_id=row[0],
-                   brewery_name=row[1],
-                   review_time=row[2],
-                   overall=row[3],
-                   review_aroma=row[4],
-                   review_appearance=row[5],
-                   review_profilename=row[6],
-                   beer_style=row[7],
-                   review_palate=row[8],
-                   review_taste=row[9],
-                   beer_name=row[10],
-                   beer_abv=row[11],
-                   beer_beerid=row[12])
+batch_size = 2000
 
 
 @db_session
 def read_csv_file(filepath):
-    with io.open(filepath) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    with open(filepath) as csv_file:
+        csv_reader = unicodecsv.reader(csv_file)
+
         # Skip first line -> header
         next(csv_reader)
-        items = []
+
+        counter = 0
         for row in csv_reader:
-            print(unicode(row))
+            brewery_id = int(row[0])
+            brewery_name = row[1]
             epoch_duration = int(row[2])
-            review_date = datetime.utcfromtimestamp(epoch_duration)
-            BeerReview(brewery_id=row[0],
-                       brewery_name=row[1],
-                       review_time=review_date,
-                       overall=float(row[3]),
-                       aroma=float(row[4]),
-                       appearance=float(row[5]),
-                       profilename=row[6],
-                       beer_style=row[7],
-                       palate=float(row[8]),
-                       taste=float(row[9]),
-                       beer_name=row[10],
-                       beer_abv=float(row[11]),
-                       beer_id=int(row[12]))
+            review_date = parse_datetime(epoch_duration)
+            overall_rating = float(row[3])
+            aroma_rating = float(row[4])
+            appearance_rating = float(row[5])
+            profilename = row[6]
+            beer_style = row[7]
+            palate_rating = float(row[8])
+            taste_rating = float(row[9])
+            beer_name = row[10]
+
+            # Have no clue what this describes
+            beer_abv = None
+            if row[11].strip():
+                beer_abv = float(row[11])
+
+            beer_id = int(row[12])
+            try:
+                BeerReview(brewery_id=brewery_id,
+                           brewery_name=brewery_name,
+                           review_time=review_date,
+                           overall=overall_rating,
+                           aroma=aroma_rating,
+                           appearance=appearance_rating,
+                           profilename=profilename,
+                           beer_style=beer_style,
+                           palate=palate_rating,
+                           taste=taste_rating,
+                           beer_name=beer_name,
+                           beer_abv=beer_abv,
+                           beer_id=beer_id)
+            except Exception as e:
+                print('Error occured for' + str(row) +
+                      ' , exception: ' + str(e))
+
+            if counter % batch_size == 0:
+                print('Committing, current count: ' + str(counter))
+                commit()
+            counter = counter + 1
+
+
+def parse_datetime(epoch_duration):
+    try:
+        return datetime.utcfromtimestamp(epoch_duration)
+    except Exception as e:
+        print(str(e))
+        return None
